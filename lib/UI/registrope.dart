500c62a30b8ce3ea7fcef6b448275a2111f677ac
mpdes <maanpival@hotmail.com>
2020-05-04 09:15:13 -0500

    Color + icon en registro

--- lib/UI/registrope.dart
+++ lib/UI/registrope.dart
@@ -1,27 +1,23 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:proyectorainbox/Const/Constants.dart';
 import 'package:proyectorainbox/bloc/personbloc.dart';
 import 'package:proyectorainbox/model/regitro.dart';
 
-
 class Registropage extends StatefulWidget {
-  Registropage({Key key}) : super (key: key);
+  Registropage({Key key}) : super(key: key);
 
-@override
-RegistropageState createState()=> RegistropageState();
+  @override
+  RegistropageState createState() => RegistropageState();
 }
 
-class RegistropageState extends State<Registropage> 
-    with SingleTickerProviderStateMixin{
+class RegistropageState extends State<Registropage>
+    with SingleTickerProviderStateMixin {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-   ProductBloc productBloc;
-   Product _product = Product(nombre: '', apellido: '', cedula: 0, placa: '');
- 
- 
- @override
+  ProductBloc productBloc;
+  Product _product = Product(nombre: '', apellido: '', cedula: 0, placa: '');
+
+  @override
   void initState() {
     super.initState();
     productBloc = ProductBloc(context);
@@ -46,95 +42,98 @@ class RegistropageState extends State<Registropage>
       productBloc.createProduct(_product);
       //TecniNavigator.goToHomeI(context);
     }
-   
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Registro de personas')),
+      appBar: AppBar(
+        title: Text(
+          'Registro de personas',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.yellow,
+      ),
       body: SingleChildScrollView(
+        padding: const EdgeInsets.all(30.0),
         child: Center(
           child: Column(
             children: <Widget>[
               Padding(
-                padding: const EdgeInsets.all(20),
+                padding: const EdgeInsets.all(5),
               ),
               TextField(
-                decoration: InputDecoration(
-                    hintText: Const.nombre,
-                    labelText: Const.nombre,
-                    icon: Icon(Icons.people),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20.0))),
-              onChanged: (valor) {
-                      _product.nombre = valor;
-  }
-  ),
+                  decoration: InputDecoration(
+                      hintText: Const.nombre,
+                      labelText: Const.nombre,
+                      icon: Icon(Icons.person_pin),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0))),
+                  onChanged: (valor) {
+                    _product.nombre = valor;
+                  }),
               //
               Padding(
                 padding: const EdgeInsets.all(20),
               ),
               TextField(
-                decoration: InputDecoration(
-                    hintText: Const.apellido,
-                    labelText: Const.apellido,
-                    icon: Icon(Icons.person_add),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20.0))),
-                         onChanged: (valor) {
-                      _product.apellido = valor;
-                         }
-              ),
+                  decoration: InputDecoration(
+                      hintText: Const.apellido,
+                      labelText: Const.apellido,
+                      icon: Icon(Icons.person_pin),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0))),
+                  onChanged: (valor) {
+                    _product.apellido = valor;
+                  }),
               //
               Padding(
                 padding: const EdgeInsets.all(20),
               ),
               TextField(
-                decoration: InputDecoration(
-                    hintText: Const.cedula,
-                    labelText: Const.cedula,
-                    icon: Icon(Icons.person_outline),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20.0))),
-                         onChanged: (valor) {
-                      _product.cedula = int.parse(valor);}
-              ),
+                  decoration: InputDecoration(
+                      hintText: Const.cedula,
+                      labelText: Const.cedula,
+                      icon: Icon(Icons.recent_actors),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0))),
+                  onChanged: (valor) {
+                    _product.cedula = int.parse(valor);
+                  }),
               //
               Padding(
                 padding: const EdgeInsets.all(20),
               ),
               TextField(
-                decoration: InputDecoration(
-                    hintText: Const.placa,
-                    labelText: Const.placa,
-                    icon: Icon(Icons.explore),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20.0))),
-             onChanged: (valor) {
-                      _product.placa = valor;
-             }
-              ),
+                  decoration: InputDecoration(
+                      hintText: Const.placa,
+                      labelText: Const.placa,
+                      icon: Icon(Icons.directions_car),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0))),
+                  onChanged: (valor) {
+                    _product.placa = valor;
+                  }),
               //
 
               Padding(
                 padding: EdgeInsets.only(top: 50.0),
               ),
-               RaisedButton(
-                      padding: new EdgeInsets.fromLTRB(40, 0, 40, 0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                      ),
-                      child: Text('Registrar'),
-                      textColor: Colors.black87,
-                      color: Colors.green[500],
-                      onPressed: _handleSubmitted),
+              RaisedButton(
+                  padding: new EdgeInsets.fromLTRB(40, 0, 40, 0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  ),
+                  child: Text('Registrar'),
+                  textColor: Colors.black87,
+                  color: Colors.orangeAccent,
+                  onPressed: _handleSubmitted),
             ],
           ),
         ),
       ),
     );
   }
-    
 }
